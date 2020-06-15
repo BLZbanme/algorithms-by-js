835034184897360309fb1daeb705250ed4864d3a
BLZbanme <swordcome@gmail.com>
2020-06-15 15:53:57 +0800

    fix KMP

--- string/kmp.js
+++ string/kmp.js
@@ -1,77 +1,36 @@
-function KMP(str, substr, next = []) {
-    let i = 1;
-    let j = 1;
-    while (i <= str.length && j <= substr.length) {
-        if (j == 0 || str[i] === substr[j]) {
-            ++i;
-            ++j;
-        }
-        else {
+var strStr = function(s, p){
+    let next = getNext(p);
+    let i = 0;
+    let j = 0;
+    let sLen = s.length;
+    let pLen = p.length;
+    while(i < sLen && j < pLen){
+        if(j == -1 || s[i] == p[j]){
+            i++;
+            j++;
+        }else{
             j = next[j];
         }
     }
-    if (j > substr.length) {
-        return i - substr.length;
-    }
-    else {
-        return 0;
-    }
-}
-
-const getNext = (substr, next = []) => {
-    let j = 1;
-    let t = 0;
-    next[1] = 0;
-    while (j < substr.length) {
-        if (t == 0 || substr[j] == substr[t]) {
-            next[j + 1] = t + 1;
-            t++;
-            j++;
-        }
-        else {
-            t = next[t];
-        }
+    if(j == pLen){
+        return i - j;
+    }else{
+        return -1;
     }
 }
 
-
-function KMP(str, substr, nextVal = []) {
-    let i = 1;
-    let j = 1;
-    while (i <= str.length && j <= substr.length) {
-        if (j == 0 || str[i] === substr[j]) {
+function getNext(str){
+    let next = new Array(str.length);
+    let i = 0, j = -1;
+    next[0] = -1;
+    while(i < str.length - 1){
+        if(j == -1 || str[i] == str[j]){
             ++i;
             ++j;
-        }
-        else {
-            j = nextVal[j];
-        }
-    }
-    if (j > substr.length) {
-        return i - substr.length;
-    }
-    else {
-        return 0;
-    }
-}
-
-const getNextVal = (substr, nextVal = []) => {
-    let j = 1;
-    let t = 0;
-    nextVal[1] = 0;
-    while (j < substr.length) {
-        if (t == 0 || substr[j] == substr[t]) {
-            if (substr[j + 1] != substr[next[j + 1]]) {
-                next[j + 1] =  t + 1;
-            }
-            else {
-                nextVal[j + 1] = nextVal[ t + 1];
-            }
-            t++;
-            j++;
-        }
-        else {
-            t = nextVal[t];
+            next[i] = j;
+        }else{
+            j = next[j];
         }
     }
+    return next;
 }
